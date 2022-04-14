c4c622d2e378d0cc0c322aba79cb2ed6b0d8a4c0
Mohammad Arefi <m-arefi@live.com>
2022-04-14 11:04:09 +0200

    Header and Datingcards components initiated

---
+++ dating-app-mern/dating-app-frontend/src/components/Header/Header.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Header.css";
+import PersonIcon from "@material-ui/icons/Person";
+import { IconButton } from "@material-ui/core";
+import { ForumIcon } from "@material-ui/icons/Forum";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <IconButton>
+        <PersonIcon fontSize="large" className="header_icon" />
+      </IconButton>
+      <img
+        className="header_logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Facebook_Logo_%282019%29.png/800px-Facebook_Logo_%282019%29.png"
+        alt="header"
+      />
+      <IconButton>
+        <ForumIcon fontSize="large" className="header_icon" />
+      </IconButton>
+    </div>
+  );
+};
+
+export default Header;
